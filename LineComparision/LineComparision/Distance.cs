ca8bb74203804f994f7c5e0681699e017485fa61
kiran1902 <kiru2kumar@gmail.com>
2022-03-19 11:01:41 +0530

    To Calculate Length of Line

--- LineComparision/LineComparision/Distance.cs
+++ LineComparision/LineComparision/Distance.cs
@@ -8,17 +8,15 @@ namespace LineComparision
 {
     public class Distance
     {
-        public void Main(String[] args)
+      
+        public double LineDistance()
         {
             //user input
-            Console.WriteLine("Enter the length of Point X1 and X2: ");
+            Console.WriteLine("Enter the length of Point X1 , X2 , Y1 and Y2 : ");
             var X1 = Convert.ToDouble(Console.ReadLine());
             var X2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter the Length of Point Y1 and Y2 : ");
             var Y1 = Convert.ToDouble(Console.ReadLine());
             var Y2 = Convert.ToDouble(Console.ReadLine());
-
 
             //Initial Formula to calculate the Distane of the Line
             var temp1 = Math.Pow((X2 - X1), 2);
@@ -26,8 +24,8 @@ namespace LineComparision
 
             //Final formula
             double Result = Math.Sqrt(temp1 + temp2);
-
-            Console.WriteLine(" The Length of Line is : " + Result);
+            Console.WriteLine("The Length of Line is", +Result);
+            return Result;
 
 
         }
--- LineComparision/LineComparision/Program.cs
+++ LineComparision/LineComparision/Program.cs
@@ -2,5 +2,5 @@
 
 Console.WriteLine("Welcome to Line Comparision Computation Program!");
 
-Distance dist = new();
-dist.Main(args);
+Distance dist = new Distance();
+double Result = dist.LineDistance();
